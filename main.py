76eacca4395ebb511ea1d3c5fec9f16c7781bafe
anngithubuser <163045604+anngithubuser@users.noreply.github.com>
2024-04-08 11:54:16 +0200

    rene did this :)

--- main.py
+++ main.py
@@ -3,9 +3,33 @@ OrderOfNumbers
 """
 # Provide your solution here
 
+n1= int(input("Please write n1: "))
+n2= int(input("Please write n2: "))
+n3= int(input("Please write n3: "))
+
+if n1<n2 and n2<n3:
+    input("numbers are ascending")
+elif n1>n2 and n2>n3:
+    input("numbers are descending")
+else:
+    if (n1%2)==0 and (n2%2)==0 and (n3%2)==0:
+        input("no specific order, but all even")
+    elif (n1%2)==1 and (n2%2)==1 and (n3%2)==1:
+        input("no specific order, but all odd")
+    else:
+        input("no specific order")
 
 
 """
 SumUp
 """
 # Provide your solution here
+n1= int(input("Please write 1st number: "))
+n2= int(input("Please write 2nd number: "))
+
+print(list(range(n1,n2)))
+
+if n1>n2:
+    input("n2 needs to be > n1")
+elif n1<0 or n2<0:
+    input("n1 and n2 need to be > 0")
